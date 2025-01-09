2f104b68a26f5658d1b5271133b2bd55fa565b0e
AyushRay06 <rayayush47@gmail.com>
2025-01-09 20:36:44 +0530

    setup-clerk-shadcnwdarkmode-nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import ModeToggle from "@/components/mode-toggle"
+import { Button } from "@/components/ui/button"
+import { SignedIn, SignInButton, UserButton, SignedOut } from "@clerk/nextjs"
+export default function Home() {
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>SignIn</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <ModeToggle />
+    </div>
+  )
+}
