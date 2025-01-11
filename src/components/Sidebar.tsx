c63d137ca3d5e73bb1e90ea2c90632483cf5cfb2
AyushRay06 <rayayush47@gmail.com>
2025-01-12 01:08:13 +0530

    auth-sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-// import { getUserByClerkId } from "@/actions/user.action" //Server action File👈
+import { getUserByClerkId } from "@/actions/user.action" //Server action File👈
 
 import { currentUser } from "@clerk/nextjs/server"
 import { SignInButton, SignUpButton } from "@clerk/nextjs"
@@ -20,13 +20,76 @@ import Link from "next/link"
 const Sidebar = async () => {
   const authUser = await currentUser()
   //Check if user exist. If do not then show them the UnAuthenticatedSidebar
-  if (!authUser) {
-    return <UnAuthenticatedSidebar />
-  } else {
-    return <div>gdcwyegvcg</div>
-  }
+  if (!authUser) return <UnAuthenticatedSidebar />
 
-  // const user = await
+  const user = await getUserByClerkId(authUser.id)
+  if (!user) return null
+  console.log(user)
+
+  return (
+    <div className="sticky top-20">
+      <Card>
+        <CardContent className="pt-6">
+          <div className="flex flex-col items-center text-center">
+            <Link
+              href={`/profile/${user.username}`}
+              className="flex flex-col items-center justify-center"
+            >
+              <Avatar className="w-20 h-20 border-2 ">
+                <AvatarImage src={user.image || "/avatar.png"} />
+              </Avatar>
+
+              <div className="mt-4 space-y-1">
+                <h3 className="font-semibold">{user.name}</h3>
+                <p className="text-sm text-muted-foreground">{user.username}</p>
+              </div>
+            </Link>
+
+            {user.bio && (
+              <p className="mt-3 text-sm text-muted-foreground">{user.bio}</p>
+            )}
+
+            <div className="w-full">
+              <Separator className="my-4" />
+              <div className="flex justify-between">
+                <div>
+                  <p className="font-medium">{user._count.following}</p>
+                  <p className="text-xs text-muted-foreground">Following</p>
+                </div>
+                <Separator orientation="vertical" />
+                <div>
+                  <p className="font-medium">{user._count.followers}</p>
+                  <p className="text-xs text-muted-foreground">Followers</p>
+                </div>
+              </div>
+              <Separator className="my-4" />
+            </div>
+
+            <div className="w-full space-y-2 text-sm">
+              <div className="flex items-center text-muted-foreground">
+                <MapPinIcon className="w-4 h-4 mr-2" />
+                {user.location || "No location"}
+              </div>
+              <div className="flex items-center text-muted-foreground">
+                <LinkIcon className="w-4 h-4 mr-2 shrink-0" />
+                {user.website ? (
+                  <a
+                    href={`${user.website}`}
+                    className="hover:underline truncate"
+                    target="_blank"
+                  >
+                    {user.website}
+                  </a>
+                ) : (
+                  "No website"
+                )}
+              </div>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
 
 export default Sidebar
