61c86e592456ac68f6a6a1ac2dc938b78c6c0ee1
AyushRay06 <rayayush47@gmail.com>
2025-01-13 06:28:38 +0530

    getPosts

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -23,3 +23,57 @@ export async function createPost(content: string, image: string) {
     return { success: false, error: "Failed to create post" }
   }
 }
+
+export async function getPosts() {
+  try {
+    const posts = await prisma.post.findMany({
+      orderBy: {
+        createdAt: "desc", //latest post first
+      },
+      //post should inclued these fields
+      //used include insted of select as these fields are from related table and not from the native one
+      include: {
+        author: {
+          //fields of author ie from user table
+          select: {
+            id: true,
+            name: true,
+            username: true,
+            image: true,
+          },
+        },
+        comments: {
+          include: {
+            author: {
+              select: {
+                id: true,
+                name: true,
+                image: true,
+                username: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: "asc", //new comments up
+          },
+        },
+        likes: {
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            likes: true,
+            comments: true,
+          },
+        },
+      },
+    })
+
+    return posts
+  } catch (error) {
+    console.log("Error in getPosts", error)
+    throw new Error("Failed to fetch posts")
+  }
+}
